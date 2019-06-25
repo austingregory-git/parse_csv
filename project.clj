601d0e51d9cbdf9cd6fe405a73e140df86e1fe2e
Austin Gregory <agregory@iastate.edu>
2019-06-25 17:54:58 -0500

    read csv into vector of vectors, read csv into vector of hash-maps. Read xlsx into vector of vectors. Attempted pdf parsing but pdfboxing is outdated(?)

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/data.csv "0.1.4"]]
+                 [org.clojure/data.csv "0.1.4"]
+                 [dk.ative/docjure "1.7.0"]
+                 [pdfboxing "0.1.13"]]
   :repl-options {:init-ns parse-csv.core})
--- src/parse_csv/core.clj
+++ src/parse_csv/core.clj
@@ -1,6 +1,9 @@
 (ns parse-csv.core
   (:require [clojure.java.io :as io]
-            [clojure.data.csv :as csv]))
+            [clojure.data.csv :as csv]
+            [dk.ative.docjure.spreadsheet :as spreadsheet]))
+
+;; [pdfboxing.text :as text]
 
 (defn foo
   "I don't do a whole lot."
@@ -13,3 +16,32 @@
   (into [] (with-open [reader (io/reader filename)]
      (doall
        (csv/read-csv reader)))))
+
+;;(def x (read-csv "example2.csv"))
+;; (.indexOf (nth x 1) "NLC Name") => 1
+;(nth x 1)
+;=> ["" "NLC Name" "Soup, Inc" "" "" "" "" "" "Invoice No" "123456"]
+;; (get (nth x 1) (.indexOf (nth x 1) "NLC Name"))
+;=> "NLC Name"
+;(get (nth x 1) (+ (.indexOf (nth x 1) "NLC Name") 1))
+;=> "Soup, Inc"
+
+(defn find-headers [filename])
+
+(defn csv-data->maps [csv-data]
+   (into [] (map hash-map
+                 (->> (first csv-data) ;; First row is the header
+                      (map keyword) ;; Drop if you want string keys instead
+                      repeat)
+                 (rest csv-data))))
+
+(csv-data->maps (read-csv reader))
+
+(defn read-rows [filename]
+  (->>
+    (spreadsheet/load-workbook filename)
+    (spreadsheet/select-sheet "Sheet1")
+    (spreadsheet/select-columns {:A :a-name :B :b-name :C :c-name :D :d-name :E :e-name :F :f-name :G :g-name :H :h-name :I :i-name})))
+
+(defn read-pdf [filename]
+  (text/extract filename))
